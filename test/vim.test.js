c730a2b7912d59e312e41dbfc94328f139c2ef27
Paul Mourer <paul.mourer@gmail.com>
2019-03-05 15:02:50 -0700

    set up mocha console, istanbul (code coverage)

---
+++ test/vim.test.js
@@ -0,0 +1,10 @@
+ const chai = require('chai')
+ const assert = chai.assert
+ const expect = chai.expect
+ const should = chai.should()
+
+ describe('chai/mocha install', function () {
+     it('worked?', function () {
+         expect(true).to.equal(true)
+     })
+ })
